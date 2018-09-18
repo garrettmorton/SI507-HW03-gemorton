adb83971e99d5b42ca823b0a32d1efb043eea554
Tori Cox <vgcox@umich.edu>
2018-09-18 16:27:36 -0400

    everything is the same now

--- magic_eight.py
+++ magic_eight.py
@@ -5,7 +5,6 @@ def get_question():
 	return question
 <<<<<<< HEAD
 =======
-
 >>>>>>> master
 answer_list = ["It is certain", "It is decidedly so.", "Without a doubt.", "Yes - definitely.",
 "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
