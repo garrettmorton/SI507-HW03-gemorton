e838504dfef097db0996cc0c5ce731fd98d983bc
Garrett Morton <gemorton@gmail.com>
2018-09-18 16:33:52 -0400

    fixing resolution issue

--- magic_eight.py
+++ magic_eight.py
@@ -2,7 +2,6 @@ import random
 
 def get_question():
 	question = input("What is your question?")
-<<<<<<< HEAD
         return str(question)
 
 def check_question(question):
@@ -25,27 +24,4 @@ question = 0
 
 while question != "quit":
     question = get_question()
-=======
-        return str(question)
 
-def check_question(question):
-    question_str = str(question)
-    if question_str[-1] != "?":
-        print("I'm sorry, I can only answer questions!")
-        return False
-    else:
-        return True
-
-answer_list = ["It is certain", "It is decidedly so.", "Without a doubt.", "Yes - definitely.",
-"You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
-"Reply hazy, try again","Ask again later.", "Better not tell you now.", "Cannot predict now.",
-"Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no", "Outlook not so good.", "Very doubtful" ]
-
-answer = answer_list[random.randrange(20)]
-print(answer)
-
-question = 0
-
-while question != "quit":
-    question = get_question()
->>>>>>> check_question
