811803fabd91485c30f90a752c5d26b4c8163b51
Garrett Morton <gemorton@gmail.com>
2018-09-18 16:11:15 -0400

    check_question

--- magic_eight.py
+++ magic_eight.py
@@ -2,4 +2,19 @@ import random
 
 def get_question():
 	question = input("What is your question?")
-	return question
+        return str(question)
+
+def check_question(question):
+    question_str = str(question)
+    if question_str[-1] != "?":
+        print("I'm sorry, I can only answer questions!")
+        return False
+    else:
+        return True
+
+question = 0
+
+while question != "quit":
+    question = get_question()
+
+    
